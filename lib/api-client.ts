6727e70895cd5b63e2f430a7c3caa1f610f143e7
Munna-Khandakar <munnashisad@gmail.com>
2026-04-16 20:16:45 +0600

    inventory added

--- lib/api-client.ts
+++ lib/api-client.ts
@@ -1,8 +1,7 @@
 import axios from "axios"
 
 const baseURL =
-  process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://163.227.239.237:9099"
-  // "https://crabbedly-blatant-randy.ngrok-free.dev"
+  process.env.NEXT_PUBLIC_API_BASE_URL ?? "https://apon-report.duckdns.org"
 
 export const apiClient = axios.create({
   baseURL,
